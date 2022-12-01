29484f45dd4da2cb0f10652c5d30c2d6f0ad5a12
devStr0ke <samueltomar@hotmail.com>
2022-12-01 15:06:40 +0100

    Images architecture + Halcyon Logo

--- components/NavBar/Navbar.tsx
+++ components/NavBar/Navbar.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Image from "next/image";
 export const Navbar = () => {
   const [scrollPosition, setScrollPosition] = useState(0);
   const handleScroll = () => {
@@ -16,9 +17,9 @@ export const Navbar = () => {
 
   const navBarClass = () => {
     if (Math.floor(scrollPosition) < 44) {
-      return "sticky top-0 bg-red-500 h-16 mx-0 flex justify-center md:bg-red-500 md:mt-11 md:h-16 md:transition-navBar md:duration-[1500ms] md:animate-slidein md:mx-16 lg:mx-24 xl:mx-36";
+      return "sticky top-0 bg-red-500 h-18 mx-0 flex justify-center md:bg-red-500 md:mt-11 md:h-16 md:transition-navBar md:duration-[1500ms] md:animate-slidein md:mx-16 lg:mx-24 xl:mx-36";
     } else {
-      return "sticky top-0 bg-red-500 h-16 mx-0 flex justify-center md:bg-red-500 md:mt-11 md:h-16 md:transition-navBar md:duration-[1500ms] md:animate-slidein xl:mx-44 md:sticky md:top-0 md:bg-blue-500 md:mx-0 lg:mx-0 xl:mx-0";
+      return "sticky top-0 bg-red-500 h-18 mx-0 flex justify-center md:bg-red-500 md:mt-11 md:h-16 md:transition-navBar md:duration-[1500ms] md:animate-slidein xl:mx-44 md:sticky md:top-0 md:bg-blue-500 md:mx-0 lg:mx-0 xl:mx-0";
     }
   };
 
@@ -26,7 +27,7 @@ export const Navbar = () => {
     <div className={navBarClass()}>
       <div className="h-16 flex items-center bg-transparent overflow-hidden">
         <div className="flex md:gap-[500px]">
-          <div>MENU</div>
+          <Image src="/static/images/halcyonFull.png" alt="logo" width="220" height="100" />
         </div>
       </div>
     </div>
