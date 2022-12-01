ad76b923fcf8e091abcd2b66a1c04f0846db11d6
devStr0ke <samueltomar@hotmail.com>
2022-12-01 19:38:54 +0100

    hero header + responsive navBar

--- components/NavBar/Navbar.tsx
+++ components/NavBar/Navbar.tsx
@@ -6,6 +6,10 @@ export const Navbar = () => {
     const position = window.pageYOffset;
     setScrollPosition(position);
   };
+  const [hamburgerController, setActive] = useState(false) //use state for hamburger state controller (x or =)
+  const toggleClass = () => {
+    setActive(!hamburgerController)
+  };
 
   useEffect(() => {
     window.addEventListener("scroll", handleScroll, { passive: true });
@@ -17,9 +21,9 @@ export const Navbar = () => {
 
   const navBarClass = () => {
     if (Math.floor(scrollPosition) < 44) {
-      return "sticky top-0 bg-white h-18 mx-0 flex justify-center md:bg-white md:mt-11 md:h-16 md:transition-navBar md:duration-[1500ms] md:animate-slidein md:mx-16 lg:mx-24 xl:mx-36";
+      return "z-[999] sticky top-0 bg-white h-16 mx-0 flex justify-center lg:bg-white lg:mt-11 lg:h-16 lg:transition-navBar lg:duration-[1500ms] lg:animate-slidein lg:mx-16 lg:mx-24 xl:mx-36";
     } else {
-      return "sticky top-0 bg-white h-18 mx-0 flex justify-center md:bg-white md:mt-11 md:h-16 md:transition-navBar md:duration-[1500ms] md:animate-slidein xl:mx-44 md:sticky md:top-0 md:bg-black md:mx-0 lg:mx-0 xl:mx-0";
+      return "z-[999] sticky top-0 bg-white h-16 mx-0 flex justify-center lg:bg-white lg:mt-11 lg:h-16 lg:transition-navBar lg:duration-[1500ms] lg:animate-slidein xl:mx-44 lg:sticky lg:top-0 lg:bg-black lg:mx-0 lg:mx-0 xl:mx-0";
     }
   };
 
@@ -30,13 +34,35 @@ export const Navbar = () => {
   }
 
   return (
-    <div className={navBarClass()}>
-      <div className="h-16 flex items-center bg-transparent overflow-hidden">
-        <div className="flex md:gap-[500px]">
-          <Image className="hidden md:flex" src={logoColor()} alt="logoMdAndUp" width="50" height="100" />
-          <Image className="flex md:hidden" src="/static/svg/halcyonLogoBlack.svg" alt="logoDownMD" width="50" height="100" />
+    <>
+      {/*NavBar*/}
+      <div className={navBarClass()}>
+        <div className="flex items-center bg-transparent overflow-hidden">
+          <div className="flex items-center h-16">
+            <Image className="hidden lg:flex" src={logoColor()} alt="logolgAndUp" width="45" height="100" />
+            <Image className="absolute top-2.2 left-6 lg:hidden" src="/static/svg/halcyonLogoBlack.svg" alt="logoDownlg" width="40" height="55" />
+            <div onClick={toggleClass} className="absolute space-y-1.5 top-7 right-6 lg:hidden">
+              <span className={hamburgerController ? 'block w-5 h-0.5 bg-black transition-transform duration-300 rotate-45 translate-y-1' : 'block w-5 h-0.5 bg-black transition-transform duration-300'}></span>
+              <span className={hamburgerController ? 'block w-5 h-0.5 bg-black transition-transform duration-300 -rotate-45 -translate-y-1' : 'block w-5 h-0.5 bg-black transition-transform duration-300'}></span>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+      {/*Mobile Menu*/}
+      <div className={hamburgerController ? 'xl:hidden top-[65px] h-full bg-white w-full fixed z-[999] transform transition-transform duration-500 translate-x-0' : 'xl:hidden top-[65px] h-full bg-white w-full fixed z-[999] transform transition-transform duration-500 -translate-x-full'}>
+        <div className="text-lg uppercase text-black flex justify-center mt-12 hover:text-purple-800">
+          <div>Reviews</div>
+        </div>
+        <div className="text-lg uppercase text-black flex justify-center mt-8 hover:text-purple-800">
+          <div>Guides</div>
+        </div>
+        <div className="text-lg uppercase text-black flex justify-center mt-8 hover:text-purple-800">
+          <div>About</div>
+        </div>
+        <div className="text-lg uppercase text-black flex justify-center mt-8 hover:text-purple-800">
+          <div>Services</div>
+        </div>
+      </div>
+    </>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,24 @@
-import Link from "next/link"
+import Link from "next/link";
+import Image from "next/image";
+import { Navbar } from "../components/NavBar/Navbar";
+
 
 export default function Home() {
   return (
-    <div className='flex h-screen justify-center items-center'>
-      <Link href="/about">
-        <div className="text-3xl hover:text-blue-500">Go to about page</div>
-      </Link>
-    </div>
+    <>
+      <div className="-z-10 relative w-full h-[100vh]">
+        <div className="-z-10">
+          <Image
+            priority
+            src="/static/images/heroImage.png"
+            layout="fill"
+            objectFit="cover"
+            objectPosition="center"
+            alt="hero image example"
+          />
+        </div>
+      </div>
+      <div className="mt-96 mb-96">Hello</div>
+    </>
   )
 }
