48537bd162677577d1a9da0a017dec1d8b9aa89f
Shannorr <samueltomar@hotmail.com>
2022-12-08 17:44:30 +0100

    fix next/image

--- components/NavBar/Navbar.tsx
+++ components/NavBar/Navbar.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import Image from 'next/image';
+import Image from "next/legacy/image";
 export const Navbar = () => {
   const [scrollPosition, setScrollPosition] = useState(0);
   const handleScroll = () => {
