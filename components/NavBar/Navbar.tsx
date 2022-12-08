62134a85e6a7893646af110b53bd8b1dbf4a692f
Shannorr <samueltomar@hotmail.com>
2022-12-08 17:59:45 +0100

    fixx image on next 13

--- components/NavBar/Navbar.tsx
+++ components/NavBar/Navbar.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import Image from "next/legacy/image";
+import Image from "next/image";
 export const Navbar = () => {
   const [scrollPosition, setScrollPosition] = useState(0);
   const handleScroll = () => {
@@ -51,92 +51,92 @@ export const Navbar = () => {
     }
   };
 
-
   const logoColor = () => {
     if (Math.floor(scrollPosition) < 44) {
       return '/static/svg/halcyonLogoBlack.svg';
     } else return '/static/svg/halcyonLogoWhite.svg';
   };
 
-  return (
-    <>
-      {/*NavBar*/}
-      <div>
-        <header className={navBarHeightController()}>
-          <div className={navBarController()}>
-            <div className='absolute h-[65px] border-b-[3px] w-[36px] border-cyan-500'></div>
-            <div className="overflow-hidden">
-              <a className='overflow-clip' href="#">
-                <Image
-                  className="hidden lg:flex"
-                  src={logoColor()}
-                  alt="logolgAndUp"
-                  width="40"
-                  height="100"
-                />
-              </a>
-              <a href='#'>
-                <Image
-                  className="lg:hidden"
-                  src="/static/svg/halcyonLogoBlack.svg"
-                  alt="logoDownlg"
-                  width="40"
-                  height="55"
-                />
-              </a>
-              <div className={leftNavBarItemsController()}>
-                <a href="#">About</a>
-                <a href="#">Reviews</a>
-                <a href="#">Guides</a>
-                <a href="#">Info</a>
-              </div>
-              <div className={rightNavBarItemsController()}>
-                <a href="#">Collab</a>
-                <a href="#">Roadmap</a>
-                <a href="#">Team</a>
-                <a href="#">Contact</a>
-              </div>
-              <div onClick={toggleClass} className="absolute space-y-1.5 top-7 left-6 lg:hidden">
-                <span
-                  className={
-                    hamburgerController
-                      ? 'block w-5 h-0.5 bg-black transition-transform duration-300 rotate-45 translate-y-1'
-                      : 'block w-5 h-0.5 bg-black transition-transform duration-300'
-                  }
-                ></span>
-                <span
-                  className={
-                    hamburgerController
-                      ? 'block w-5 h-0.5 bg-black transition-transform duration-300 -rotate-45 -translate-y-1'
-                      : 'block w-5 h-0.5 bg-black transition-transform duration-300'
-                  }
-                ></span>
-              </div>
+  return <>
+    {/*NavBar*/}
+    <div>
+      <header className={navBarHeightController()}>
+        <div className={navBarController()}>
+          <div className="absolute h-[65px] border-b-[3px] w-[36px] border-cyan-500"></div>
+          <div className="overflow-hidden">
+            <a className="overflow-clip" href="#">
+              <Image
+                className="hidden lg:flex"
+                src={logoColor()}
+                alt="logolgAndUp"
+                width="40"
+                height="100"
+                style={{
+                  maxWidth: "100%",
+                  height: "auto"
+                }} />
+            </a>
+            <a href="#">
+              <Image
+                className="lg:hidden"
+                src="/static/svg/halcyonLogoBlack.svg"
+                alt="logoDownlg"
+                width="40"
+                height="55"
+                style={{
+                  maxWidth: "100%",
+                  height: "auto"
+                }} />
+            </a>
+            <div className={leftNavBarItemsController()}>
+              <a href="#">About</a>
+              <a href="#">Reviews</a>
+              <a href="#">Guides</a>
+              <a href="#">Info</a>
+            </div>
+            <div className={rightNavBarItemsController()}>
+              <a href="#">Collab</a>
+              <a href="#">Roadmap</a>
+              <a href="#">Team</a>
+              <a href="#">Contact</a>
+            </div>
+            <div onClick={toggleClass} className="absolute space-y-1.5 top-7 left-6 lg:hidden">
+              <span
+                className={
+                  hamburgerController
+                    ? 'block w-5 h-0.5 bg-black transition-transform duration-300 rotate-45 translate-y-1'
+                    : 'block w-5 h-0.5 bg-black transition-transform duration-300'
+                }></span>
+              <span
+                className={
+                  hamburgerController
+                    ? 'block w-5 h-0.5 bg-black transition-transform duration-300 -rotate-45 -translate-y-1'
+                    : 'block w-5 h-0.5 bg-black transition-transform duration-300'
+                }></span>
             </div>
           </div>
-        </header>
+        </div>
+      </header>
+    </div>
+    {/*Mobile Menu*/}
+    <div
+      className={
+        hamburgerController
+          ? 'lg:hidden top-[65px] h-full bg-white w-full border-t-[0.01px] border-gray-300 border-opacity-40 fixed z-[799] transform transition-transform duration-500 translate-y-0'
+          : 'lg:hidden top-[65px] h-full bg-white w-full fixed z-[799] transform transition-transform duration-500 -translate-y-full'
+      }>
+      <div className="text-lg font-bold uppercase text-black flex justify-center mt-12 hover:text-cyan-400">
+        <div>Reviews</div>
       </div>
-      {/*Mobile Menu*/}
-      <div
-        className={
-          hamburgerController
-            ? 'lg:hidden top-[65px] h-full bg-white w-full border-t-[0.01px] border-gray-300 border-opacity-40 fixed z-[799] transform transition-transform duration-500 translate-y-0'
-            : 'lg:hidden top-[65px] h-full bg-white w-full fixed z-[799] transform transition-transform duration-500 -translate-y-full'
-        }
-      >
-        <div className="text-lg font-bold uppercase text-black flex justify-center mt-12 hover:text-cyan-400">
-          <div>Reviews</div>
-        </div>
-        <div className="text-lg font-bold uppercase text-black flex justify-center mt-8 hover:text-cyan-400">
-          <div>Guides</div>
-        </div>
-        <div className="text-lg font-bold uppercase text-black flex justify-center mt-8 hover:text-cyan-400">
-          <div>About</div>
-        </div>
-        <div className="text-lg font-bold uppercase text-black flex justify-center mt-8 hover:text-cyan-400">
-          <div>Services</div>
-        </div>
+      <div className="text-lg font-bold uppercase text-black flex justify-center mt-8 hover:text-cyan-400">
+        <div>Guides</div>
       </div>
-    </>
-  );
+      <div className="text-lg font-bold uppercase text-black flex justify-center mt-8 hover:text-cyan-400">
+        <div>About</div>
+      </div>
+      <div className="text-lg font-bold uppercase text-black flex justify-center mt-8 hover:text-cyan-400">
+        <div>Services</div>
+      </div>
+    </div>
+  </>;
 };
