2095526ece35952d2a6ad590787095ef9010332d
devStr0ke <samueltomar@hotmail.com>
2022-12-08 18:11:40 +0100

    Updated NavBar

--- components/NavBar/Navbar.tsx
+++ components/NavBar/Navbar.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 export const Navbar = () => {
-  const [scrollPosition, setScrollPosition] = useState(0);
+  const [scrollPosition, setScrollPosition] = useState(0); 
   const handleScroll = () => {
     const position = window.pageYOffset;
     setScrollPosition(position);
@@ -13,6 +13,7 @@ export const Navbar = () => {
 
   useEffect(() => {
     window.addEventListener('scroll', handleScroll, { passive: true });
+    handleScroll();//appeller la fonction pour definir la position du scroll si user recharge la page
 
     return () => {
       window.removeEventListener('scroll', handleScroll);
@@ -21,33 +22,33 @@ export const Navbar = () => {
 
   const navBarHeightController = () => {
     if (Math.floor(scrollPosition) > 44) {
-      return 'top-[0px] bg-transparent w-full flex justify-center fixed z-[800] duration-100';
+      return 'top-[0px] bg-transparent w-full flex justify-center fixed z-[800]';
     } else {
-      return 'top-[0px] bg-transparent w-full flex justify-center fixed z-[800] duration-100 lg:top-11';
+      return 'top-[0px] bg-transparent w-full flex justify-center absolute z-[800] duration-100 lg:top-11';
     }
   };
 
   const navBarController = () => {
     if (Math.floor(scrollPosition) > 44) {
-      return 'h-[65px] flex items-center overflow-hidden justify-center z-[800] bg-white relative w-full lg:bg-black lg:mx-44 lg:animate-slidein lg:transition-navBar lg:duration-[1850ms] lg:mx-0';
+      return 'h-[65px] flex items-center overflow-hidden justify-center z-[800] bg-white relative w-full lg:bg-black lg:mx-44 lg:animate-slidein lg:transition-navBar lg:duration-[900ms] lg:mx-0';
     } else {
-      return 'h-[65px] flex items-center justify-center overflow-hidden z-[800] bg-white relative w-full lg:mx-24 xl:mx-36 lg:animate-slidein lg:transition-navBar lg:duration-[1200ms]';
+      return 'h-[65px] flex items-center justify-center overflow-hidden z-[800] bg-white relative w-full lg:mx-24 xl:mx-36 lg:rounded-md lg:animate-slidein lg:transition-navBar lg:duration-[1200ms]';
     }
   };
 
   const leftNavBarItemsController = () => {
     if (Math.floor(scrollPosition) > 44) {
-      return 'hidden font-bold text-white absolute top-5 left-6 h-[45px] lg:flex gap-8 lg:ml-10 xl:ml-24';
+      return 'hidden font-bold text-white absolute top-5 lg:left-2 xl:left-24 h-[45px] lg:flex gap-8 lg:ml-10 xl:ml-0';
     } else {
-      return 'hidden font-bold text-black absolute top-5 left-6 h-[45px] lg:flex gap-8 lg:ml-10 xl:ml-24';
+      return 'hidden font-bold text-black absolute top-5 lg:left-2 xl:left-24 h-[45px] animate-hideNavBarContent lg:flex gap-8 lg:ml-10 xl:ml-0';
     }
   };
 
   const rightNavBarItemsController = () => {
     if (Math.floor(scrollPosition) > 44) {
-      return 'hidden font-bold text-white absolute top-5 right-6 h-[45px] lg:flex gap-8 lg:mr-10 xl:mr-24';
+      return 'hidden font-bold text-white absolute top-5 lg:right-2 xl:right-24 h-[45px] lg:flex gap-8 lg:mr-10 xl:mr-0';
     } else {
-      return 'hidden font-bold text-black absolute top-5 right-6 h-[45px] lg:flex gap-8 lg:mr-10 xl:mr-24';
+      return 'hidden font-bold text-black absolute top-5 lg:right-2 xl:right-24 h-[45px] animate-hideNavBarContent lg:flex gap-8 lg:mr-10 xl:mr-0';
     }
   };
 
@@ -65,8 +66,8 @@ export const Navbar = () => {
         <header className={navBarHeightController()}>
           <div className={navBarController()}>
             <div className='absolute h-[65px] border-b-[3px] w-[36px] border-cyan-500'></div>
-            <div className="overflow-hidden">
-              <a className='overflow-clip' href="#">
+            <div className="">
+              <a className='overflow-clip animate-hideImage' href="#">
                 <Image
                   className="hidden lg:flex"
                   src={logoColor()}
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,18 +7,31 @@ module.exports = {
   theme: {
     extend: {
       transitionProperty: {
-        'navBar' : 'margin-left, margin-right, border-radius, height'
+        'navBar' : 'margin-left, margin-right, border-radius, height',
+        'width' : 'width',
       },
       keyframes: {
         slidein : {
-          '0%': { height: '0.2rem', marginRight: '50rem', marginLeft: '50rem' },
+          '0%': { height: '0.2rem', marginRight: '50rem', marginLeft: '50rem', display: 'none' },
           '50%': { height: '0.2rem' },
           '75%': { height: '0.2rem' },
           '85%': { height: '0.2rem' },
-        }
+        },
+        hideImage: {
+          '0%': { opacity: '0' },
+          '50%': { opacity: '0' },
+          '95%': { opacity: '1' },
+        },
+        hideNavBarContent: {
+          '0%': { opacity: '0' },
+          '50%': { opacity: '0' },
+          '95%': { opacity: '1' },
+        },
       },
       animation: {
-        slidein: 'slidein 1.2s'
+        slidein: 'slidein 1.2s',
+        hideImage: 'hideImage 1.9s',
+        hideNavBarContent: 'hideNavBarContent 1.2s',
       }
     },
   },
