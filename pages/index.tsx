66deac6c7b3699198fab1d52ab8291c92996abbe
Shannorr <samueltomar@hotmail.com>
2022-12-11 11:55:11 +0100

    start adding logo of roadmäp

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
-import { Header } from "../components/Header/Header";
-
+import { Header } from '../components/Header/Header';
 
 export default function Home() {
   return (
     <>
       <Header />
-      <div className="mt-96 mb-96">test</div>
+      <div className="pt-96 pb-96 bg-red-100 text-black">test</div>
     </>
-  )
+  );
 }
