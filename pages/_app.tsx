0bdef873546e5ba40969f536a75aed35dc28b2be
COELHO Samuel <samueltomar@hotmail.com>
2022-12-16 15:00:10 +0100

    feature/team added fonts in global css file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import { Navbar } from '../components/NavBar/Navbar'
 import type { AppProps } from 'next/app'
-import Image from "next/image";
+
 import { Roadmap } from '../components/Core/Roadmap/Roadmap';
 
 export default function App({ Component, pageProps }: AppProps) {
