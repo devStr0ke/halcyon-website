6bd5c7b3132a88ed3e9b79025637cc2beaa1cc05
devStr0ke <samueltomar@hotmail.com>
2023-01-07 16:49:27 +0100

    Overview fixes + add title + text

--- components/Content/First.tsx
+++ components/Content/First.tsx
@@ -87,15 +87,24 @@ export const First = () => {
           className="z-20 absolute h-[100vh] w-full flex justify-start lg:pl-36 lg:pr-[35vw] sm:pl-14 sm:pr-[20vw] animate-imageTransition"
         >
           <div className="bg-no-repeat bg-left bg-cover bg-[url('/static/images/HeroLab.png')] lg:rounded-lg w-full">
+            <div className='h-[100vh] absolute flex justify-start items-center pl-6 sm:pl-24'>
+              <div className='text-4xl font-medium text-gray-400'>Our</div>
+            </div>
+            <div className='h-[100vh] absolute flex justify-start items-center pl-6 sm:pl-24 top-12'>
+              <div className='textBoxShadow text-5xl font-bold'>Mission</div>
+            </div>
+            <div className='h-[100vh] w-[300px] sm:w-96 absolute flex justify-start items-center pl-6 sm:pl-24 top-44'>
+              <div className='text-md font-regular'>Web3 has immense potential and can disrupt our societies in the long run, but for now it is mostly dangerous. We believe that reliable and trustworthy infrastructures are essential to create a healthy foundation for this new decentralized internet.</div>
+            </div>
           </div>
         </div>
         <div className="z-20 relative sm:h-[15vh] w-full flex justify-start xl:pr-[30vw] lg:pr-[20vw] sm:pr-[15vw]">
-          <div className="w-full textBoxShadow px-6 lg:text-8xl sm:text-7xl text-6xl font-semibold sm:py-3 saira flex justify-start sm:justify-end uppercase animate-imageTransition translate-x-0">
+          <div className="w-full textBoxShadow px-6 lg:text-8xl sm:text-7xl text-6xl font-bold sm:py-3 saira flex justify-start sm:justify-end uppercase animate-imageTransition translate-x-0">
             Building the
           </div>
         </div>
         <div className="z-20 relative sm:h-[15vh] w-full flex justify-start xl:pl-16 lg:pl-14 sm:pl-4">
-          <div className="w-full textBoxShadow px-6 sm:px-0 lg:text-8xl sm:text-7xl text-6xl font-semibold sm:py-3 saira flex justify-start sm:justify-start uppercase animate-imageTransition">
+          <div className="w-full textBoxShadow px-6 sm:px-0 lg:text-8xl sm:text-7xl text-6xl font-bold sm:py-3 saira flex justify-start sm:justify-start uppercase animate-imageTransition">
             future of Web3
           </div>
         </div>
