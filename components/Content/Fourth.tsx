ee33822080077f0fee48908bc2452efff75da90f
devStr0ke <samueltomar@hotmail.com>
2023-01-09 15:35:34 +0100

    Team section finished

--- components/Content/Fourth.tsx
+++ components/Content/Fourth.tsx
@@ -1,38 +1,20 @@
 import Image from 'next/image';
 import React, { useEffect, useState, useRef } from 'react';
+import { useDraggable } from "react-use-draggable-scroll";
 export const Fourth = () => {
+  const ref =  useRef<HTMLDivElement>() as React.MutableRefObject<HTMLInputElement>;
+  const { events } = useDraggable(ref);
+
   return (
     <>
-      <div className='hidden md:flex md:overflow-x-hidden -translate-x-'>
-        <div className="w-full h-[340px] flex justify-around">
-          <div className="h-[340px] w-[25vw] bg-transparent flex justify-center">
-            <div className="flex justify-center bg-no-repeat h-[340px] bg-center bg-contain bg-[url('/static/images/Team/thounyTeam.png')] rounded-lg w-full"></div>
-          </div>
-          <div className="h-[340px] w-[25vw] bg-transparent">
-            <div className="bg-no-repeat h-[340px] bg-center bg-contain bg-[url('/static/images/Team/aurelineTeam.png')] rounded-lg w-full"></div>
-          </div>
-        </div>
-        <div className="hidden lg:h-[35px] lg:w-full"></div>
-        <div className="w-full h-[340px] flex justify-around">
-          <div className="h-[340px] w-[25vw] bg-transparent">
-            <div className="bg-no-repeat h-[340px] bg-center bg-contain bg-[url('/static/images/Team/devStr0keTeam.png')] rounded-lg w-full"></div>
-          </div>
-          <div className="h-[340px] w-[25vw] bg-transparent">
-            <div className="bg-no-repeat h-[340px] bg-center bg-contain bg-[url('/static/images/Team/matTeam.png')] rounded-lg w-full"></div>
-          </div>
-          <div className="h-[340px] w-[25vw] bg-transparent">
-            <div className="bg-no-repeat h-[340px] bg-center bg-contain bg-[url('/static/images/Team/sajuloTeam.png')] rounded-lg w-full"></div>
-          </div>
-        </div>
-        <div className="hidden lg:h-[35px] lg:w-full"></div>
-        <div className="w-full h-[340px] flex justify-around">
-          <div className="h-[340px] w-[25vw] bg-transparent">
-            <div className="bg-no-repeat h-[340px] bg-center bg-contain bg-[url('/static/images/Team/shanTeam.png')] rounded-lg w-full"></div>
-          </div>
-          <div className="h-[340px] w-[25vw] bg-transparent">
-            <div className="bg-no-repeat h-[340px] bg-center bg-contain bg-[url('/static/images/Team/virgule.png')] rounded-lg w-full"></div>
-          </div>
-        </div>
+      <div {...events} ref={ref} className='flex w-full space-x-0 overflow-x-scroll no-scrollbar bg-transparent'>
+        <div className="flex-none bg-no-repeat h-96 bg-center bg-contain bg-[url('/static/images/Team/thounyTeam.png')] rounded-lg w-[400px]"></div>
+        <div className="flex-none bg-no-repeat h-96 bg-center bg-contain bg-[url('/static/images/Team/aurelineTeam.png')] rounded-lg w-[400px]"></div>
+        <div className="flex-none bg-no-repeat h-96 bg-center bg-contain bg-[url('/static/images/Team/devStr0keTeam.png')] rounded-lg w-[400px]"></div>
+        <div className="flex-none bg-no-repeat h-96 bg-center bg-contain bg-[url('/static/images/Team/shanTeam.png')] rounded-lg w-[400px]"></div>
+        <div className="flex-none bg-no-repeat h-96 bg-center bg-contain bg-[url('/static/images/Team/matTeam.png')] rounded-lg w-[400px]"></div>
+        <div className="flex-none bg-no-repeat h-96 bg-center bg-contain bg-[url('/static/images/Team/sajuloTeam.png')] rounded-lg w-[400px]"></div>
+        <div className="flex-none bg-no-repeat h-96 bg-center bg-contain bg-[url('/static/images/Team/virgule.png')] rounded-lg w-[400px]"></div>
       </div>
     </>
   );
