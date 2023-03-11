92d01a23fd61c6205e5f6fc06ef3958c03c0e8ad
Shannorr <samueltomar@hotmail.com>
2023-03-11 15:33:52 +0100

    test

--- pages/dispenser.tsx
+++ pages/dispenser.tsx
@@ -1,22 +1,10 @@
 // import { useDispenserStore, useMonkeyStore } from "../store/store";
-import { SessionContextProvider } from '@supabase/auth-helpers-react';
 import { TEST_ADDRESS } from '../backend/dispenser/config';
 // import useStoreContractInfo from '../backend/dispenser/useStoreContractInfo';
 import useStoreUserInfo from '../backend/dispenser/useStoreUserInfo';
-import { useState } from 'react';
-import type { AppProps } from 'next/app';
-import { createBrowserSupabaseClient, Session } from '@supabase/auth-helpers-nextjs';
-import Home from '.';
 
-function Dispenser({Component, pageProps}:  AppProps<{initialSession: Session,}>) {
-    useStoreUserInfo(TEST_ADDRESS);
-    const [supabase] = useState(() => createBrowserSupabaseClient())
+export default function Dispenser() {
+  useStoreUserInfo(TEST_ADDRESS);
 
-    return (
-        <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
-            <h1 className="text-4xl mt-40 text-cyan-500">njr</h1>
-            <Home />
-        </SessionContextProvider>
-    )
+  return <>{/* <h1 className="text-4xl mt-40 text-cyan-500">njr</h1> */}</>;
 }
-export default Dispenser;
