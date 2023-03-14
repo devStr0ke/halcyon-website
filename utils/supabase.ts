2c94ac9ee8473df51c3305dcb2b902ef2849f3de
Theo <samueltomar@hotmail.com>
2023-03-14 07:03:53 +0100

    add signup signin test

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,6 +1,13 @@
+import { createServerComponentSupabaseClient } from '@supabase/auth-helpers-nextjs';
 import { createClient } from '@supabase/supabase-js';
+import { headers, cookies } from "next/headers";
 
-export default createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+// createClient(
+//   process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+// );
+
+export default createServerComponentSupabaseClient({
+  headers,
+  cookies
+});
