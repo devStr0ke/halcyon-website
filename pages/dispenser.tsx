8edaca318ca6d5c836107c46d9a539d1fcc4f0e2
Matthias Monnier <samueltomar@hotmail.com>
2023-03-20 21:07:32 +0100

    Update dispenser ui

--- pages/dispenser.tsx
+++ pages/dispenser.tsx
@@ -39,20 +39,44 @@ export default function Dispenser({ data }: { data: any }) {
 
   return (
     <WalletKitProvider>
-      <div className="w-screen h-screen py-40 bg-gray-100 flex flex-col items-center justify-between">
-        <div className="flex w-full justify-end px-10">
-          <Connect />
+      <div className="w-screen h-screen py-40 bg-gray-100 flex flex-col items-center justify-start">
+        <div className="text-red-600 pb-12">
+          Beware, the obtained NFTs live on Sui devnet, which is frequently reset. This will make
+          you lose your entire wallet! So remember to register your wetlist ASAP.
         </div>
 
         <div className="w-full flex justify-around">
-          <div className="bg-white p-6 rounded-lg shadow-md w-1/3">
-            <h2 className="text-2xl font-bold mb-4">Left Block</h2>
-            <p>Content for the left block goes here.</p>
+          <div className="bg-white p-6 rounded-lg shadow-md w-2/5 h-96 flex flex-col justify-between">
+            <h2 className="text-2xl font-bold text-center">Dispenser Drawing</h2>
+            <div className="w-full grid grid-cols-2 gap-4">
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Buy
+              </button>
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Recycle
+              </button>
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Swap
+              </button>
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Claim
+              </button>
+            </div>
           </div>
 
-          <div className="bg-white p-6 rounded-lg shadow-md w-1/3">
-            <h2 className="text-2xl font-bold mb-4">Right Block</h2>
-            <p>Content for the right block goes here.</p>
+          <div className="w-2/5">
+            <div className="flex flex-col justify-between px-10">
+              <p>Current/Upcoming Batch: </p>
+              <p className="text-center">X Bottles Availabe</p>
+              <p className="text-center">Y Bottles Minted</p>
+              <p className="text-center">Z Filled Minted</p>
+            </div>
+            <div className="flex w-full justify-between px-10">
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Connect Discord
+              </button>
+              <Connect />
+            </div>
           </div>
         </div>
       </div>
