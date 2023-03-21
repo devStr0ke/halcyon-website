29b07e148cc4cc0e1078473bb38d096a9a44c17d
devStr0ke <samueltomar@hotmail.com>
2023-03-21 23:39:37 +0100

    useOnScreen hook for anim simplification

---
+++ hooks/useOnScreen.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+
+export default function useOnScreen(ref) {
+  const [isIntersecting, setIntersecting] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIntersecting(entry.isIntersecting);
+      },
+      {
+        threshold: 0
+      }
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, [ref]);
+
+  return isIntersecting;
+}
