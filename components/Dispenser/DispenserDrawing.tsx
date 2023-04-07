c3157ae1005a582c0efcf6f0980a648061039b44
devStr0ke <samueltomar@hotmail.com>
2023-04-07 18:07:58 +0200

    bouton positioning (needs rework)

--- components/Dispenser/DispenserDrawing.tsx
+++ components/Dispenser/DispenserDrawing.tsx
@@ -68,46 +68,57 @@ const DispenserDrawing: React.FC<DispenserDrawingProps> = ({ roles }) => {
   }
 
   return (
-    <div className="">
-      <div className="bg-no-repeat bg-center bg-cover bg-[url('/static/images/products/distributeur.png')] p-6 rounded-lg w-full h-[90vh] flex flex-col justify-end">
-        <div className="w-full grid grid-cols-2 gap-4">
-          <button
-            disabled={getBatchOrNot(dispenser, user) === BatchOrNot.Closed}
-            onClick={() => handleBuy(dispenser)}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded -skew-y-[2deg] disabled:bg-slate-400">
-            Buy
-          </button>
-          <button
-            disabled={emptyBottleIds.length < 5}
-            onClick={() => handleRecycle()}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded -skew-y-[2deg] disabled:bg-slate-400">
-            Recycle
-          </button>
-          <button
-            disabled={ticketIds.length === 0}
-            onClick={() => handleSwap()}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded -skew-y-[2deg] disabled:bg-slate-400">
-            Swap
-          </button>
-          <button
-            disabled={
-              roles.filter((r) => r.enthusiast === false).filter((r) => r.claimed === false)
-                .length === 0 &&
-              roles.filter((r) => r.enthusiast === true).filter((r) => r.claimed === false)
-                .length === 0
-            }
-            onClick={() => handleClaim()}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded -skew-y-[2deg] disabled:bg-slate-400">
-            Claim
-          </button>
-          <button
-            disabled={filledBottleIds.length === 0}
-            onClick={() => handleRegister()}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded -skew-y-[2deg] disabled:bg-slate-400">
-            Register
-          </button>
+    <div className='relative w-full'>
+      <div className="bg-no-repeat bg-center bg-cover bg-[url('/static/images/products/distributeur.png')] w-full h-[90vh]">
+        <div className='absolute w-full h-56 bottom-0'>
+          <div className='absolute inset-0 -skew-y-[2deg]'>
+            <button
+              disabled={getBatchOrNot(dispenser, user) === BatchOrNot.Closed}
+              onClick={() => handleBuy(dispenser)}
+              className="absolute text-2xl hover:text-cyan-700 text-cyan-500 font-bold py-2 px-4 rounded disabled:text-slate-400"
+              style={{ top: '10%', left: '45%', transform: 'translate(-50%, -50%)' }}
+            >
+              Buy
+            </button>
+            <button
+              disabled={emptyBottleIds.length < 5}
+              onClick={() => handleRecycle()}
+              className="absolute text-2xl hover:text-cyan-700 text-cyan-500 font-bold py-2 px-4 rounded disabled:text-slate-400"
+              style={{ top: '12%', left: '71%', transform: 'translate(-50%, -50%)' }}
+            >
+              Recycle
+            </button>
+            <button
+              disabled={ticketIds.length === 0}
+              onClick={() => handleSwap()}
+              className="absolute text-2xl hover:text-cyan-700 text-cyan-500 font-bold py-2 px-4 rounded disabled:text-slate-400"
+              style={{ top: '40%', left: '45%', transform: 'translate(-50%, -50%)' }}
+            >
+              Swap
+            </button>
+            <button
+              disabled={
+                roles.filter((r) => r.enthusiast === false).filter((r) => r.claimed === false)
+                  .length === 0 &&
+                roles.filter((r) => r.enthusiast === true).filter((r) => r.claimed === false)
+                  .length === 0
+              }
+              onClick={() => handleClaim()}
+              className="absolute text-2xl hover:text-cyan-700 text-cyan-500 font-bold py-2 px-4 rounded disabled:text-slate-400"
+              style={{ top: '40%', left: '71%', transform: 'translate(-50%, -50%)' }}
+            >
+              Claim
+            </button>
+          </div>
         </div>
       </div>
+      <button
+        disabled={filledBottleIds.length === 0}
+        onClick={() => handleRegister()}
+        className="relative w-full hover:bg-cyan-700 bg-cyan-500 font-bold text-white py-2 px-4 rounded disabled:bg-slate-400"
+        >
+          Register
+      </button>
     </div>
   );
 };
