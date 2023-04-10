5af4122dd66339fad41b7c7e4eed8a823dab09db
devStr0ke <samueltomar@hotmail.com>
2023-04-10 16:02:11 +0200

    originbyte management

--- originbyte-js-sdk/src/client/index.ts
+++ originbyte-js-sdk/src/client/index.ts
@@ -1,10 +1,10 @@
 export * from "./parsers";
 
-export * from "./orderbook/OrderbookFullClient";
-export * from "./orderbook/OrderbookReadClient";
+//export * from "./orderbook/OrderbookFullClient";
+//export * from "./orderbook/OrderbookReadClient";
 
-export * from "./safe/SafeReadClient";
-export * from "./safe/SafeFullClient";
+//export * from "./safe/SafeReadClient";
+//export * from "./safe/SafeFullClient";
 
 export * from "./NftClient";
 export * from "./types";
