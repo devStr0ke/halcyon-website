9e8d368c756dff26908bf7b1d4601386c932264c
Samuel Coelho <samueltomar@hotmail.com>
2023-04-21 18:01:35 +0200

    Fix on wetlist bar position

--- components/DispenserPage/Dispenser/Connection/Connection.tsx
+++ components/DispenserPage/Dispenser/Connection/Connection.tsx
@@ -1,9 +1,7 @@
-import { useUserStore } from '../../../../store/userStore';
 import DiscordLogin from './DiscordLogin';
 import SuiConnect from './SuiConnect';
 
 const Connection = () => {
-  const { isWetlisted } = useUserStore((state) => state);
 
   return (
     <>
@@ -11,11 +9,6 @@ const Connection = () => {
         <DiscordLogin />
         <SuiConnect />
       </div>
-      {isWetlisted && (
-        <div className="bg-green-200 w-full border border-green-400 rounded-md mx-0 p-0 px-3">
-          <p className='text-center text-green-700'>Congratulations, you are wetlisted!</p>
-        </div>
-      )}
     </>
   );
 };
